c66366c0cabd3dcb0bf44b7804e61df752eb3008
Aleix Ripoll <aleix.ripoll@gmail.com>
2024-02-04 12:24:54 +0100

    traduir search placeholder

--- src/layouts/Search.tsx
+++ src/layouts/Search.tsx
@@ -71,7 +71,7 @@ export default function SearchBar({ searchList }: Props) {
     <div className="min-h-[45vh]">
       <input
         className="form-input w-full text-center"
-        placeholder="Type here to Search posts"
+        placeholder="Escribe aquí para buscar artículos"
         type="text"
         name="search"
         value={inputVal}
