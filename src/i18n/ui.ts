4ddf5bc57237def31b47afe406b1876685226956
Aleix Ripoll <ripollpe@adobe.com>
2024-05-26 11:51:24 +0200

    i18n draft

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+    en: 'English',
+    es: 'Castellano',
+    ca: 'Català',
+  };
+  
+  export const defaultLang = 'es';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.about': 'About',
+    },
+    es: {
+      'nav.home': 'Inicio',
+      'nav.about': 'Acerca de',
+    },
+    ca: {
+        'nav.home': 'Inici',
+        'nav.about': 'Quant a',
+      },
+  } as const;
+
+  export const showDefaultLang = false;
+  
